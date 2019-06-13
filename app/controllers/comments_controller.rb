8fbc658e49be42079affdb6921fea5dd81436fe4
rkaku <greenapple728@icloud.com>
2019-06-13 12:19:23 +0000

    Force sign-in before posting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,22 @@ class CommentsController < ApplicationController
   before_action :set_article
   
   def create
-    @comment = @article.comments.build(comment_params)
-    @comment.user = current_user
+    unless current_user
+      flash[:alert] = "Please sign in or sign up first."
+      redirect_to new_user_session_path
     
-    if @comment.save
-      flash[:success] = "Comment has been created."
     else
-      flash.now[:alert] = "Comment has not been created."
+      @comment = @article.comments.build(comment_params)
+      @comment.user = current_user
+      
+      if @comment.save
+        flash[:success] = "Comment has been created."
+      else
+        flash.now[:alert] = "Comment has not been created."
+      end
+      
+      redirect_to article_path(@article)
     end
-    
-    redirect_to article_path(@article)
   end
   
   private
